834642f2c85432ca6948be758f995b497dbfc747
emad-aisha <emadaisha2024@gmail.com>
2026-08-01 03:59:14 +0300

    more

--- Assets/Game/! Shaders/SpriteLayerManager.cs
+++ Assets/Game/! Shaders/SpriteLayerManager.cs
@@ -1,9 +1,20 @@
 using UnityEngine;
+using UnityEditor;
 
 public class SpriteLayerManager : MonoBehaviour {
 
     void Start() {
         SpriteRenderer[] sprites = GetComponentsInChildren<SpriteRenderer>();
+
+        for (int i = 0; i < sprites.Length; i++) {
+            sprites[i].sortingOrder = (int)-sprites[i].transform.position.z * 10;
+        }
+    }
+
+
+    [MenuItem("Sorting Layers/Readjust")]
+    static void AdjustSortingLayers() {
+        SpriteRenderer[] sprites = FindObjectsByType<SpriteRenderer>();
 
         for (int i = 0; i < sprites.Length; i++) {
             sprites[i].sortingOrder = (int)-sprites[i].transform.position.z * 10;
